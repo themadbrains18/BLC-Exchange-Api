ef60b512f0d3b5eacfc8432a8ee23976b83c57f9
surindermdb <surinderkumar.mdb@gmail.com>
2023-03-01 12:59:19 +0530

    cahnges for assets transfer to wallet

--- app/models/assets.model.js
+++ app/models/assets.model.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const UserAssets = sequelize.define("assets", {
     userID:{
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     accountType: {
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     token_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     balance:{
