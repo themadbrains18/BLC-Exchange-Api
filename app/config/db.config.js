8310290b47a756ced4a858f1357fbe486695b2c8
themadbrains18 <dev@themadbrains.com>
2023-03-03 10:56:15 +0530

    last login

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "localhost", //localhost
   USER: "root",
-  PASSWORD: '@dmin#2019',// "tmb@2018#2",
+  PASSWORD: '@Dmin#2019$',// "tmb@2018#2",
   DB: "blc",
   dialect: "mysql",
   pool: {
@@ -13,7 +13,7 @@ module.exports = {
   SMTP_HOST: "smtp.gmail.com",
   SMTP_PORT: '587',
   SMTP_SECURE_TYPE: 'Tls',
-  SMTP_USER: 'surinderdhanjufss@gmail.com',// 'surinderdhanjufss@gmail.com',
-  SMTP_PASSWORD: 'lmmyrkpjsebbdgoj',// 'lmmyrkpjsebbdgoj',
+  SMTP_USER: 'blcexchange.net@gmail.com',// 'surinderdhanjufss@gmail.com',
+  SMTP_PASSWORD: 'yeaqfufvtyybzggm',// 'lmmyrkpjsebbdgoj',
 };
 
